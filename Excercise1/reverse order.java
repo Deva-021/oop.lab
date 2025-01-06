94c44466aafd0b9095f6c075cdc445637eadb4c6
Deva-021 <deva2402124@gmail.com>
2025-01-06 22:51:53 +0530

    Add files via upload

---
+++ Excercise1/reverse order.java
@@ -0,0 +1,9 @@
+public class reversenumbers {
+
+    public static void main(String[] args) {
+        // Loop to print numbers from 10 to 1 in reverse order
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
